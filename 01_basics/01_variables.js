5794df476b3acbb5a742317152566379b7db6434
URMIK J PATEL <114993425+PRABHU-ujp6@users.noreply.github.com>
2025-02-11 07:36:51 +0000

    new_commit_config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accId = 6;
+var accName = "Akan";
+let accState = "Gujarat";
+
+console.table([accId, accName, accState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+
+
+
+console.log(typeof "Akan");
+console.log(typeof null);
+console.log(typeof undefined);
