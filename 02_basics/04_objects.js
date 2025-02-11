a5ab911a504f10bea1ec32dfc5bf3f7c7b3f82ae
URMIK J PATEL <114993425+PRABHU-ujp6@users.noreply.github.com>
2025-02-11 12:49:23 +0000

    Objects part 2 done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,36 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Urmik"
+tinderUser.email = "Ujp@1234.com"
+
+// console.log((tinderUser));
+
+const regularUser = {
+    email: "abc@gmail.com",
+    fullname: {
+        newFullName: {
+            firstname: "Akan",
+            lastname: "Patel"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.newFullName.lastname);
+
+const obj1 = { 1: "a", 2: "b"}
+const obj2 = { 3: "c", 4: "d"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("email"));
+
