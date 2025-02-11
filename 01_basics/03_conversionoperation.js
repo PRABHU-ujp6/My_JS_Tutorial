9d209d2054ba046fe23ab416958888c0c4b0f604
URMIK J PATEL <114993425+PRABHU-ujp6@users.noreply.github.com>
2025-02-11 09:49:49 +0000

    conversion operations

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,43 @@
+// let score =  "64ujp";
+
+// console.log(typeof score);
+
+// let newName = Number(score);
+// console.log(typeof newName);
+// console.log(newName);
+
+
+// let bigNumber = 215478963254178252n
+// console.log(typeof bigNumber);
+
+// let userOne = {
+//     email: 'ujp@gmail.com'
+// }
+
+// let usertwo = userOne
+
+// usertwo.email = "akan@gmail.com"
+
+// console.log(userOne);
+// console.log(usertwo);
+
+
+const gameName = new String("bivbcibj")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const newStringone = gameName.slice(-6, 4)
+console.log(newStringone);
+
+const newStringTwo = "   jccioubucb   "
+console.log(newStringTwo.trim());
+
+
+
+
