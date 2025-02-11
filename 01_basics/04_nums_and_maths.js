ff81580e7077c6bf712af4d66d8fcb7cbdc72508
URMIK J PATEL <114993425+PRABHU-ujp6@users.noreply.github.com>
2025-02-11 10:13:23 +0000

    maths and nums done

---
+++ 01_basics/04_nums_and_maths.js
@@ -0,0 +1,31 @@
+// const score = 400
+
+// const newScore = new Number(score)
+// console.log(newScore);
+
+// console.log(newScore.toString());
+// console.log(typeof newScore);
+// console.log(newScore.toFixed(2));
+
+//+++++++++++++++++++Maths++++++++++++++++++++++
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,5,6,3,8));
+console.log(Math.max(4,6,5,8,7));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const max = 20
+const min = 10
+console.log(Math.round(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
